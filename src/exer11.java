3ef241fa877cbd913ee6a9061866a262211ea85d
douglas_barbosa151 <douglasbarbosa.db10@gmail.com>
2026-06-18 21:27:31 -0300

    oi

---
+++ src/exer11.java
@@ -0,0 +1,25 @@
+public class exer11 {
+    public static void main(String[] args) {
+
+        int[][] m = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+
+// Imprimir matriz
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                System.out.print(m[i][j] + "	");
+            }
+            System.out.println();
+        }
+
+// Diagonal principal: i == j
+        int diag = 0;
+        for (int i = 0; i < m.length; i++) {
+            diag += m[i][i];
+        }
+        System.out.println("Diagonal: " + diag);  // 1+5+9 = 15
+    }
+}
